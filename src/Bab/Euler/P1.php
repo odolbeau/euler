8e4602d2979db7eb560f50190d2d22cff862deaf
Olivier Dolbeau <contact@odolbeau.fr>
2014-07-20 17:44:56 +0200

    Solved the first problem

--- src/Bab/Euler/P1.php
+++ src/Bab/Euler/P1.php
@@ -8,6 +8,12 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ * get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
 class P1 extends Command
 {
     protected function configure()
@@ -19,5 +25,32 @@ class P1 extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $multiples = [];
+
+        $i = 1;
+        $product = 0;
+        while ($product < 1000) {
+            $multiples[] = $product;
+            $product = $i * 3;
+            $i++;
+        }
+
+        $output->writeln(sprintf('Found %d multiples of 3 below 1000', count($multiples)));
+
+        $i = 1;
+        $product = 0;
+        while ($product < 1000) {
+            $multiples[] = $product;
+            $product = $i * 5;
+            $i++;
+        }
+
+        $output->writeln(sprintf('Found %d multiples of 3 and 5 below 1000', count($multiples)));
+
+        $multiples = array_unique($multiples);
+
+        $sum = array_sum($multiples);
+
+        $output->writeln(sprintf('<info>Sum: %d</info>', $sum));
     }
 }
